ba1454e5fc37e628814ba3d9787759f715cc5ee1
Robin Karlsson <robinkarlsson@Robins-MacBook-Air.local>
2021-11-19 13:06:03 +0100

    Made the json function from 33-pets use promises instead.

--- javascript/dag-15/34-promises/script.js
+++ javascript/dag-15/34-promises/script.js
@@ -4,6 +4,8 @@ PROMISES 101
 
 */
 
+
+/*
 const teacherIsNice = false;
 
 const willWeGetTheAnswersToTheExam  = () => {
@@ -32,3 +34,57 @@ willWeGetTheAnswersToTheExam()
 	alert("Promise Unfulfilled");
 	console.log("Reason:" ,err);
 });
+*/
+
+
+const getJSON = (url) => {
+
+	return new Promise((resolve, rejeect) => {
+			// Create a new XML Http Request
+	const request = new XMLHttpRequest();
+
+	// Attach a event listener to the request
+	request.addEventListener('readystatechange', () => {
+		// Check if readyState is 4 (=== DONE)
+		if (request.readyState === 4) {
+
+			// Was request successful (200 = OK)?
+			if (request.status === 200) {
+				// Request returned successful!
+				console.log("Request was OK");
+
+				// take a STRING and PARSE it into a JavaScript Object (Array)
+				const data = JSON.parse(request.responseText);
+
+				resolve(data)
+
+			} else {
+				// Something went wrong with the request
+				console.log("Request was *NOT* OK!");
+
+				rejeect("ERROR! Could not get DATA")
+			}
+		}
+	});
+
+	// Set request to GET data from url
+	request.open('GET', url);
+
+	// Send request
+	request.send();
+
+	// Done?
+	console.log("Request sent!");
+
+	})
+
+}
+
+
+getJSON('data/dogs.json')
+	.then(dogs => {
+		console.log("GOT DOGS", dogs)
+	})
+	.catch(err => {
+		console.log(err)
+	})
